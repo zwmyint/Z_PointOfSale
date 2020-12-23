38a7dac2938182cc761db16d9aee50fb1a3f7058
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2020-12-23 15:21:40 +0800

    3rd commit

---
+++ PointOfSale/Controllers/AddOnController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PointOfSale.Controllers
+{
+    public class AddOnController : Controller
+    {
+        //// GET: AddOn
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: AddOn
+        public ActionResult PageCharts()
+        {
+            return View();
+        }
+
+
+        // GET: AddOn
+        public ActionResult PageTables()
+        {
+            return View();
+        }
+    }
+}
